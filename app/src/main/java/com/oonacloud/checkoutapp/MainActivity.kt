526baf3e38d7f75b276e197f99369d01211d9697
David LAVERDA <david.laverda@aavamobile.com>
2020-03-25 16:22:45 +0100

    fix: clean code

--- app/src/main/java/com/oonacloud/checkoutapp/MainActivity.kt
+++ app/src/main/java/com/oonacloud/checkoutapp/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.oonacloud.checkoutapp
 
-import android.content.ActivityNotFoundException
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
-import android.net.Uri
-import android.provider.Browser
 import org.jetbrains.anko.alert
 
 
@@ -17,28 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val packageName = "com.oonacloud.mockupapp"
-
-        // Click listener for button widget
         button.setOnClickListener{
-            // Launch the app programmatically
             launchApp(packageName)
         }
-
     }
 
-
-    // Custom method to launch an app
     private fun launchApp(packageName: String) {
         val pm = applicationContext.packageManager
         val intent:Intent? = pm.getLaunchIntentForPackage(packageName)
         intent?.addCategory(Intent.CATEGORY_LAUNCHER)
-
         try {
             applicationContext.startActivity(intent)
         } catch (e: Exception) {
-            // Application was just removed?
             alert("Unable to bring the Oona application to foreground $packageName => startActivity(intent) : $e") {
                 title = "Alert"
                 yesButton { }
@@ -46,9 +32,4 @@ class MainActivity : AppCompatActivity() {
             }.show()
         }
     }
-}
-
-// Extension function to show toast message
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
